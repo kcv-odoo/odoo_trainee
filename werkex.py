792bbe15ca62da1cd2143915fad974463ec032d3
kcv-odoo <77771734+kcv-odoo@users.noreply.github.com>
2021-01-27 11:54:28 +0530

    Add files via upload

---
+++ werkex.py
@@ -0,0 +1,5 @@
+from werkzeug.wrappers import Response
+
+def application(environ, start_response):
+    response = Response('Hello World!', mimetype='text/plain')
+    return response(environ, start_response)
